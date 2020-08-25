79b3b6896a68737cb143eec31031227bcef20ece
LaksCastro <lakhsmycastro336@gmail.com>
2020-08-25 01:12:44 +0000

    add keep alive function

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const app = require('express')();
+const axios = require("axios");
 
 app.get('/', (req, res) => res.send('Server is up.'));
 
 module.exports = () => {
   app.listen(3000);
+  
+  function keepAlive() {
+    setTimeout(async function() {
+      await axios.get(
+        `https://bom-dia-princesas.lakscastro.repl.co`
+      );
+
+      keepAlive();
+    }, 5 * 60 * 1000);
+  }
+  keepAlive();
 }
